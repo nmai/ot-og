d0fe7a7adaa487ad722094cb4a30979ac8581ab7
Nick Mai <nick@pathship.com>
2017-12-26 19:23:06 -0800

    add complex op test and clean up old tests

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,47 +1,37 @@
 'use strict'
 
 import { expect } from 'chai'
-
-let Core = require('./lib/core').Core
-
-let Interfaces = require('./lib/interfaces'),
-    Operation = Interfaces.Operation,
-    Processor = Interfaces.Processor
-
-let Models = require('./lib/models'),
-    BaseOp = Models.BaseOp,
-    DeleteOp = Models.DeleteOp,
-    WriteOp = Models.WriteOp
-
-let TextProcessor = require('./lib/processors/text').TextProcessor
-
-// -----------------------------
-
 import { StoreManager } from './store-manager'
 import {
   createInsertAction,
-  createDeleteAction
+  createDeleteAction,
 } from './actions'
+import {
+  InsertAction, INSERT,
+  DeleteAction, DELETE
+} from './interfaces/actions.d'  // gahhh ts/jest is so annoying
 
 describe('[Core] Action Creators (Operations)', () => {
 
   it('InsertAction', () => {
-
-    expect(createInsertAction(0, '')).to.equal
+    expect(createInsertAction(3, 'asdf')).to.deep.equal(<InsertAction>{
+      type: INSERT,
+      index: 3,
+      text: 'asdf'
+    })
   })
 
-  it ('Operations have types', () => {
-    let base = new BaseOp(),
-        del = new DeleteOp(),
-        write = new WriteOp()
-
-    expect(base.type).to.equal('BaseOp')
-    expect(del.type).to.equal('DeleteOp')
-    expect(write.type).to.equal('WriteOp')
+  it ('DeleteAction', () => {
+    expect(createDeleteAction(3, 3)).to.deep.equal(<DeleteAction>{
+      type: DELETE,
+      index: 3,
+      delta: 3
+    })
   })
+
 })
 
-describe('[Core] Operating on a live Store', () => {
+describe('[Core] Atomic operations on a live Store', () => {
   let sm: StoreManager
 
   beforeEach(() => {
@@ -88,31 +78,31 @@ describe('[Core] Operating on a live Store', () => {
     })
 
   })
+})
+
+describe('[Core] Complex operations on a live Store', () => {
+  let sm: StoreManager
+
+  beforeEach(() => {
+    sm = new StoreManager({
+      text: '0000',
+      version: 0
+    })
+  })
 
   describe ('Chaining', () => {
+
     it ('6x insert/delete operations consecutively', () => {
+      sm.store.dispatch(createInsertAction(1, '*'))
+      sm.store.dispatch(createDeleteAction(1, 1))
+      sm.store.dispatch(createInsertAction(4, '**'))
+      sm.store.dispatch(createDeleteAction(6, 2))
+      sm.store.dispatch(createInsertAction(4, '****'))
+      sm.store.dispatch(createDeleteAction(5, 3))
 
+      expect(sm.store.getState().text).to.equal('*0***')
     })
-    it ('deprecated', () => {
-      let x = '0000'
-      let textProcessor = new TextProcessor(x)
-      
-      let write1at1 = new WriteOp(1, '*')
-      let write2at4 = new WriteOp(4, '**')
-      let write4at4 = new WriteOp(4, '****')
-      let del1at1 = new DeleteOp(1, 1)
-      let del2at6 = new DeleteOp(6, 2)
-      let del3at5 = new DeleteOp(5, 3)
 
-      textProcessor.apply(write1at1) ;console.log(textProcessor.snapshot) // 0*000
-      textProcessor.apply(del1at1) ;console.log(textProcessor.snapshot) // *000
-      textProcessor.apply(write2at4) ;console.log(textProcessor.snapshot) // *000**
-      textProcessor.apply(del2at6) ;console.log(textProcessor.snapshot) // *000
-      textProcessor.apply(write4at4) ;console.log(textProcessor.snapshot) // *000****
-      textProcessor.apply(del3at5) ;console.log(textProcessor.snapshot) // *0***
-      
-      expect(textProcessor.snapshot).to.equal('*0***')
-    })
   })
 
   it ('should resolve a conflicting change response', () => {
